1980143a50046b0495ba9d1d163e4e4cf6d08ee5
nithin <nithinv@dlt.sg>
2022-06-11 15:11:05 +0530

    bf working code

--- brain-fuck-compiler/main.cpp
+++ brain-fuck-compiler/main.cpp
@@ -10,49 +10,46 @@ Machine::Machine(char *code) {
      this->length = strlen(code);
 };
 
-Machine::~Machine() {}
+Machine::~Machine() {
+     delete [] code;
+}
 
 void Machine::readChar() {
      scanf("%d", &buf);
      memory[dp] = (unsigned char)buf;
 }
 
-void Machine::putChar() { printf("%d ", (int)memory[dp]); }
+void Machine::putChar() { printf("%c", (int)memory[dp]); }
 
 void Machine::excute() {
-     while (ip < length) {
+     int brace_cout = 1;
+     while (ip != length) {
           lexer();
           ip++;
      }
 }
 
 void Machine::brOpen() {
-          int depth = 1;
-          while (depth != 0) {
-               ip--;
-               if (code[ip] == '[')
-                    depth++;
-
-               else if (code[ip] == ']')
-                    depth--;
-          }
-
-          }
-
-
-void Machine::brClose() {
-
-     int depth = 1;
-     while (depth != 0) {
-          ip--;
+     int brace_count = 1;
+     while (brace_count != 0) {
+          ++ip;
           if (code[ip] == '[')
-               depth++;
-
+               ++brace_count;
           else if (code[ip] == ']')
-               depth--;
+               --brace_count;
      }
 }
 
+void Machine::brClose() {
+     int brace_count = 1;
+     while (brace_count != 0) {
+          --ip;
+          if (code[ip] == '[')
+               --brace_count;
+          else if (code[ip] == ']')
+               ++brace_count;
+     }
+}
 
 void Machine::lexer() {
      switch (code[ip]) {
@@ -81,18 +78,21 @@ void Machine::lexer() {
           break;
 
      case '[':
-          brOpen();
+          if (memory[dp] == 0)
+               brOpen();
           break;
 
      case ']':
-          brClose();
+          if (memory[dp] != 0)
+               brClose();
           break;
      }
 }
 
 int main() {
-     char arr[] = "++  > ++ [<+ >-]<.";
-     Machine *machine = new Machine(arr);
-     machine->excute();
-     return 0;
+  char arr[] = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.++++++"
+               "+..+++.>>.<-.<.+++.------.--------.>>+.>++.";
+  Machine *machine = new Machine(arr);
+  machine->excute();
+  return 0;
 }
